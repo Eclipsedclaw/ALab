3291ad6a044451dda4c40b087450bdf87b3d91c1
Jiancheng Zeng <37788723+Eclipsedclaw@users.noreply.github.com>
2022-10-18 15:02:08 -0400

    Update Setup_ALab_ubuntu.sh

--- Setup_ALab_ubuntu.sh
+++ Setup_ALab_ubuntu.sh
@@ -18,3 +18,15 @@ sudo mkdir geant4
 
 #setup ssh
 sudo iptables -t nat -A PREROUTING -p tcp -d 192.168.1.242 --dport 1412 -j DNAT --to-destination 192.168.1.242:1412       #port forwarding
+
+#install geant4
+
+#install root
+cd /opt
+mkdir root
+cd root
+git clone --branch latest-stable --depth=1 https://github.com/root-project/root.git root_src
+mkdir root_build root_install && cd root_build
+cmake -DCMAKE_INSTALL_PREFIX=../root_install ../root_src # && check cmake configuration output for warnings or errors
+cmake --build . -- install -j4 # if you have 4 cores available for compilation
+source ../root_install/bin/thisroot.sh # or thisroot.{fish,csh}
