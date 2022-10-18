2dadbf3d23bca736b71c94883bed97b322ff1597
Jiancheng Zeng <37788723+Eclipsedclaw@users.noreply.github.com>
2022-10-18 11:37:20 -0400

    Update Setup_ALab_ubuntu.sh

--- Setup_ALab_ubuntu.sh
+++ Setup_ALab_ubuntu.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+#packages installation
+sudo apt install net-tools    #to use ifconfig
+sudo apt install gparted    #install UI surface for disk management
+
+#mount HDD, only need to run these when you have new hardwares
+sudo apt install gparted    #install UI surface for disk management
+sudo fdisk -l   #list all the hardware
+sudo mkdir HDD
+sudo mount /dev/sda1 /HDD   #mount hard disk to certain directory
+
+#get a link of HDD to your home directory
+sudo ln -s /HDD ~/
+
+#install geant4
+sudo mkdir geant4
+
+#setup ssh
+sudo iptables -t nat -A PREROUTING -p tcp -d 192.168.1.242 --dport 1412 -j DNAT --to-destination 192.168.1.242:1412       #port forwarding
