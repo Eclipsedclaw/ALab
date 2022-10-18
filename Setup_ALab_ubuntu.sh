a3747624ee9ca10b91b139f4defa2eb154b6503f
Jiancheng Zeng <37788723+Eclipsedclaw@users.noreply.github.com>
2022-10-18 15:43:34 -0400

    Update Setup_ALab_ubuntu.sh

--- Setup_ALab_ubuntu.sh
+++ Setup_ALab_ubuntu.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 #packages installation
+sudo apt update
+sudo apt upgrade
+sudo apt-get update
+sudo apt-get upgrade
 sudo apt install net-tools    #to use ifconfig
 sudo apt install gparted    #install UI surface for disk management
+sudo apt install git
+sudo apt install cmake
+sudo apt-get install build-essential
+sudo apt-get install libx11-dev
+sudo apt-get install libxpm-dev
+sudo apt-get install libxft-dev
+sudo apt-get install -y libxext-dev
 
 #mount HDD, only need to run these when you have new hardwares
 sudo apt install gparted    #install UI surface for disk management
@@ -23,10 +34,10 @@ sudo iptables -t nat -A PREROUTING -p tcp -d 192.168.1.242 --dport 1412 -j DNAT 
 
 #install root
 cd /opt
-mkdir root
+sudo mkdir root
 cd root
-git clone --branch latest-stable --depth=1 https://github.com/root-project/root.git root_src
-mkdir root_build root_install && cd root_build
-cmake -DCMAKE_INSTALL_PREFIX=../root_install ../root_src # && check cmake configuration output for warnings or errors
-cmake --build . -- install -j4 # if you have 4 cores available for compilation
-source ../root_install/bin/thisroot.sh # or thisroot.{fish,csh}
+sudo git clone --branch latest-stable --depth=1 https://github.com/root-project/root.git root_src
+sudo mkdir root_build root_install && cd root_build
+sudo cmake -DCMAKE_INSTALL_PREFIX=../root_install ../root_src # && check cmake configuration output for warnings or errors
+sudo cmake --build . -- install -j4 # if you have 4 cores available for compilation
+sudo source ../root_install/bin/thisroot.sh # or thisroot.{fish,csh}
